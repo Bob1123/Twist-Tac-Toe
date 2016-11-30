ff8b776a3dc3c18032fbf29b192f58c2bb33f9c9
Bob Krueger <bobk172079@gmail.com>
2016-11-30 12:26:31 -0500

    GamePiece Clone

--- GamePiece.java
+++ GamePiece.java
@@ -9,15 +9,38 @@ public class GamePiece extends GameObject {
 	// ---------------------------------------------------------------------------------- Constructors
 	
 	public GamePiece(PieceType type) {
-		
+		setType(type);
 	}
 	
 	// ---------------------------------------------------------------------------------- Methods
+	
+	public GamePiece clone() {
+		return new GamePiece(getType());
+	}
 	
 	public void draw(Graphics g) {
 		drawFillOval(g, type.getColor(), getX()+getWidth()/10, getY()+getHeight()/10, getWidth()*8/10, getHeight()*8/10);
 	}
 	
 	// ---------------------------------------------------------------------------------- Getters and Setters
+	
+	public PieceType getType() {
+		return type;
+	}
+
+	public void setType(PieceType type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "GamePiece [type=" + type + ", x=" + getX() + ", y=" + getY() + "]";
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		// Figure this out later
+		return true;
+	}
    
 }
