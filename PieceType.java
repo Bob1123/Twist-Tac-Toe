d91adfb9dbf07f5ee78d611a3be8595d2f9e7a20
Bob Krueger <bobk172079@gmail.com>
2016-11-30 11:47:18 -0500

    Added Enumeration for PieceType:

--- PieceType.java
+++ PieceType.java
@@ -1,4 +1,25 @@
+import java.awt.Color;
+
 public enum PieceType {
-	YELLOW,
-	BLACK;	
+	YELLOW (Color.YELLOW),
+	BLACK (Color.BLACK);
+	
+	// ---------------------------------------------------------------------------------- Properties
+	
+	private final Color color;
+	
+	// ---------------------------------------------------------------------------------- Constructor
+	
+	PieceType(Color color) {
+		this.color = color;
+	}
+	
+	// ---------------------------------------------------------------------------------- Methods
+	
+	// ---------------------------------------------------------------------------------- Getters and Setters
+	
+	public Color getColor() {
+		return color;
+	}
+	
 }
