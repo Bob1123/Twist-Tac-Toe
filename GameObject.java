42dbebcaeee51bcbe01fa4901703b612a443aa7a
Bob Krueger <bobk172079@gmail.com>
2016-11-29 22:48:03 -0500

    Fix

--- GameObject.java
+++ GameObject.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public abstract class GameObject implements Drawable {
   
   //---------------------------------------------------Properties
@@ -40,49 +39,4 @@ public abstract class GameObject implements Drawable {
   public int getHeight(){
     return this.height;
   }
-  
-=======
-public abstract class GameObject implements Drawable{
-
-	//---------------------------------------------------Properties
-	private int x, y, width, height;
-
-	//---------------------------------------------------Getters and Setters
-	public void setX(int x){
-		this.x = x; 
-	}
-
-	public int getX(){
-		return this.x;
-	}
-
-	public void setY(int y){
-		this.y = y; 
-	}
-
-	public int getY(){
-		return this.y;
-	}
-
-	//Width is always non negative
-	public void setWidth(int width){
-		if(width <= 0) this.width = 0; 
-		else this.width = width;
-	}
-
-	public int getWidth(){
-		return this.width;
-	}
-
-	//Height is always non negative
-	public void setHeight(int height){
-		if(height <= 0) this.height = 0;
-		else this.height = height;
-	}
-
-	public int getHeight(){
-		return this.height;
-	}
-
->>>>>>> 417c88d96f715e54fad569bb9f90277d6f637d8b
 }
