b2e9802b86188804f66b75687a989e18177c9e58
Bob Krueger <bobk172079@gmail.com>
2016-11-30 20:07:45 -0500

    You can now draw everything!

--- CompBoard.java
+++ CompBoard.java
@@ -1,10 +1,11 @@
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class CompBoard extends GameObject {
 	
 	// ---------------------------------------------------------------------------------- Properties
 	
-	private GamePiece[][] board;
+	private GamePiece[][] board = new GamePiece[3][3];
 	private TwistArrow[] arrows;
 	
 	// ---------------------------------------------------------------------------------- Constructors
@@ -16,7 +17,24 @@ public class CompBoard extends GameObject {
 	// ---------------------------------------------------------------------------------- Methods
 	
 	public void draw(Graphics g) {
-		
+		g.setColor(Color.BLACK);
+		g.drawRect(getX(), getY(), getWidth(), getHeight());
+		g.drawLine(getX()+getWidth()/3, getY(), getX()+getWidth()/3, getY()+getHeight());
+		g.drawLine(getX()+getWidth()*2/3, getY(), getX()+getWidth()*2/3, getY()+getHeight());
+		g.drawLine(getX(), getY()+getHeight()/3, getX(), getY()+getHeight()/3);
+		g.drawLine(getX(), getY()+getHeight()*2/3, getX(), getY()+getHeight()*2/3);
+		for(GamePiece[] row : board) {
+			for(GamePiece p : row) {
+				if(p != null) {
+					p.draw(g);
+				}
+			}
+		}
+		for(TwistArrow t : arrows) {
+			if(!t.isUsed()) {
+				t.draw(g);
+			}
+		}
 	}
 	
 	// ---------------------------------------------------------------------------------- Getters and Setters
