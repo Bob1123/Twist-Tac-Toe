2b84836a6539448ce129db0fee705bd7e88ea585
Bob Krueger <bobk172079@gmail.com>
2016-11-30 19:54:46 -0500

    Whoops

--- GameObject.java
+++ GameObject.java
@@ -77,17 +77,6 @@ public abstract class GameObject implements Drawable {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + height;
-		result = prime * result + width;
-		result = prime * result + x;
-		result = prime * result + y;
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
