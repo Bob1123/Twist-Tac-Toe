2ee3d0984e3743ca33d9063a02df01cee8ed8786
Bob Krueger <bobk172079@gmail.com>
2016-12-01 20:34:18 -0500

    Tried to make a Tester...Something is failing miserably

--- Tester.java
+++ Tester.java
@@ -1,9 +1,28 @@
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Tester {
-
+public class Tester extends JPanel {
+	
+	GameBoard playField = new GameBoard(100, 100, 500, 500);
+	
+	public Tester() {
+		JFrame window = new JFrame("Twist-Tac-Toe");
+		window.setBounds(0, 0, 700, 700);
+		window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+		window.add(this);
+		window.setVisible(true);		
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        playField.draw(g);
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Time to test!");
-		
+		new Tester();
 	}
 
 }
